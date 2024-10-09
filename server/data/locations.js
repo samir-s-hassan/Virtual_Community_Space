111f2a022701a341a895d9f0a749f718ac45242f
Samir Hassan <hsamir347@gmail.com>
2024-10-09 12:24:59 -0400

    created locations data

--- server/data/locations.js
+++ server/data/locations.js
@@ -1,5 +1,44 @@
 const locationsData = [
-
+  {
+    location_id: 1,
+    name: "Spotify Camp Nou",
+    address: "C. d'Arístides Maillol, 12",
+    city: "Barcelona",
+    state: "Catalonia",
+    image: "https://www.fcbarcelona.com/photo-resources/2021/08/09/276ad270-e5c6-453d-8d9f-212417ad7cb3/Camp-Nou-3.jpg?width=1200&height=750",
+  },
+  {
+    location_id: 2,
+    name: "Stadio San Siro",
+    address: "Piazzale Angelo Moratti",
+    city: "Milan",
+    state: "Lombardy",
+    image: "https://media.tacdn.com/media/attractions-splice-spp-674x446/12/3f/b8/0c.jpg",
+  },
+  {
+    location_id: 3,
+    name: "MetLife Stadium",
+    address: "1 MetLife Stadium Dr",
+    city: "East Rutherford",
+    state: "New Jersey",
+    image: "https://www.nrg.com/assets/images/pages/case-studies/metlife-stadium/image.jpg",
+  },
+  {
+    location_id: 4,
+    name: "Lumen Field",
+    address: "800 Occidental Ave S",
+    city: "Seattle",
+    state: "Washington",
+    image: "https://s1.ticketm.net/dam/a/6ba/cca88b1c-9704-49ea-83ff-23d0e4c246ba_1665171_RETINA_PORTRAIT_3_2.jpg",
+  },
+  {
+    location_id: 5,
+    name: "Mercedes-Benz Stadium",
+    address: "1 AMB Dr NW",
+    city: "Atlanta",
+    state: "Georgia",
+    image: "https://cdn.prod.website-files.com/5ddedb3aabcf2c01a3f45461/64b4bfdf7de034a04efd75f1_contact_hero.jpg",
+  },
 ];
 
 export default locationsData;
