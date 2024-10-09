5881c162712d4f51a7279379f1916e8a4835775b
Samir Hassan <hsamir347@gmail.com>
2024-10-09 12:37:50 -0400

    created events data

--- server/data/events.js
+++ server/data/events.js
@@ -1,5 +1,148 @@
 const eventsData = [
+  // Events for Camp Nou
+  {
+    event_id: 1,
+    title: "Champions League Final",
+    description: "The biggest European football showdown of the year.",
+    date: "2024-05-28",
+    location_id: 1,
+    image:
+      "https://www.fcbarcelona.com/fcbarcelona/photo/2019/05/01/f1c3e30e-9f9f-495d-87dd-46564906f3e4/mini_2019-05-01_FCBvsLIVERPOOL_01.jpg",
+  },
+  {
+    event_id: 2,
+    title: "El Clasico",
+    description: "Fierce rivalry between FC Barcelona and Real Madrid.",
+    date: "2024-03-01",
+    location_id: 1,
+    image:
+      "https://prod-media.beinsports.com/image/1683757375171_1c672605-3ab5-4c1a-aebf-b3514dc10d66.jpg",
+  },
+  {
+    event_id: 3,
+    title: "FC Barcelona vs. Atletico Madrid",
+    description:
+      "A thrilling La Liga match between Barcelona and Atletico Madrid.",
+    date: "2024-11-22",
+    location_id: 1,
+    image:
+      "https://www.fcbarcelona.com/fcbarcelona/photo/2023/04/23/c8f60b98-b8a9-4d0c-85e0-35f8f5ffb84f/mini__GP29803.jpg",
+  },
 
+  // Events for Stadio San Siro
+  {
+    event_id: 4,
+    title: "AC Milan vs. Inter Milan Derby",
+    description: "The highly anticipated Milan Derby.",
+    date: "2024-10-12",
+    location_id: 2,
+    image:
+      "https://i.guim.co.uk/img/media/758103c5dae7de3f2ea455aceb23c1d1d1aaac4e/0_186_2048_1229/master/2048.jpg?width=480&dpr=1&s=none",
+  },
+  {
+    event_id: 5,
+    title: "Serie A Match: AC Milan vs. Juventus",
+    description: "A major Serie A match-up with Juventus.",
+    date: "2024-04-21",
+    location_id: 2,
+    image:
+      "https://www.rossoneriblog.com/wp-content/uploads/2022/10/MilanJuve_9.jpg",
+  },
+  {
+    event_id: 6,
+    title: "Concert: Coldplay",
+    description: "Coldplay performs live at the San Siro.",
+    date: "2024-06-25",
+    location_id: 2,
+    image: "https://pbs.twimg.com/media/EcBobQAWAAAYcsH?format=jpg&name=large",
+  },
+
+  // Events for MetLife Stadium
+  {
+    event_id: 7,
+    title: "Summer Music Fest",
+    description: "An exciting music festival with top artists.",
+    date: "2024-07-20",
+    location_id: 3,
+    image:
+      "https://www.app.com/gcdn/presto/2023/05/15/PAPP/48b0e521-7436-41ce-a4e0-abe5875d8e60-FrankTurner.jpg?crop=1279,720,x0,y236&width=1279&height=639&format=pjpg&auto=webp",
+  },
+  {
+    event_id: 8,
+    title: "NFL Kickoff Game",
+    description: "The New York Giants season opener.",
+    date: "2024-09-10",
+    location_id: 3,
+    image:
+      "https://giantswire.usatoday.com/wp-content/uploads/sites/67/2021/01/1294320241.jpg?w=1000&h=600&crop=1",
+  },
+  {
+    event_id: 9,
+    title: "Monster Jam",
+    description: "Monster trucks showcasing amazing stunts.",
+    date: "2024-10-05",
+    location_id: 3,
+    image:
+      "https://cdn.abcotvs.com/dip/images/10823210_062321-wtvd-monster-jam-AM-vid.jpg?w=1600",
+  },
+
+  // Events for Lumen Field
+  {
+    event_id: 10,
+    title: "Seattle Seahawks Home Opener",
+    description: "The Seahawks take on rivals in their season opener.",
+    date: "2024-09-08",
+    location_id: 4,
+    image:
+      "https://www.stadiumsofprofootball.com/wp-content/uploads/2016/07/century18950.jpg",
+  },
+  {
+    event_id: 11,
+    title: "Sounders FC Match",
+    description: "The Seattle Sounders play an important MLS game.",
+    date: "2024-05-15",
+    location_id: 4,
+    image:
+      "https://images.mlssoccer.com/image/private/t_q-best/mls-sea-prd/q6yudsjvusssdij2pabx.jpg",
+  },
+  {
+    event_id: 12,
+    title: "Concert: Taylor Swift",
+    description: "Taylor Swift live at Lumen Field.",
+    date: "2024-08-18",
+    location_id: 4,
+    image:
+      "https://www.movin925.com/wp-content/uploads/2023/07/Taylor-Swift-at-Lumen-Field-194.jpg",
+  },
+
+  // Events for Mercedes-Benz Stadium
+  {
+    event_id: 13,
+    title: "Atlanta Falcons vs. New Orleans Saints",
+    description: "NFL action between two fierce rivals.",
+    date: "2024-11-10",
+    location_id: 5,
+    image:
+      "https://res.cloudinary.com/atlanta/images/w_1024,h_768,c_scale/f_auto,q_auto/v1596482070/newAtlanta.com/AtlantaMercedesBenzStadiumInterior-1-scaled/AtlantaMercedesBenzStadiumInterior-1-scaled.jpg?_i=AA",
+  },
+  {
+    event_id: 14,
+    title: "Peach Bowl",
+    description: "A top college football bowl game.",
+    date: "2024-12-31",
+    location_id: 5,
+    image:
+      "https://footballstadiumdigest.com/wp-content/uploads/2017/09/Mercedes-Benz-Stadium-football.jpg",
+  },
+  {
+    event_id: 15,
+    title: "Concert: Beyoncé",
+    description: "Beyoncé performs live in Atlanta.",
+    date: "2024-07-14",
+    location_id: 5,
+    image:
+      "https://c8.alamy.com/comp/W04J8G/beyonce-performs-during-super-bowl-xlvii-halftime-at-the-mercedes-benz-superdome-in-new-orleans-on-february-3-2013-in-new-orleans-upidavid-tulis-W04J8G.jpg",
+  },
 ];
 
 export default eventsData;
